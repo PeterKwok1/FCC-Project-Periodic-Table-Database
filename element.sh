20010beb2a4c01381dbec42751b4333ffdbd327a
Peter Kwok <peter.kwok94@gmail.com>
2024-02-07 19:57:59 -0500

    feat: Conditional message

--- element.sh
+++ element.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Please provide an element as an argument."
+
+if [[ ! $1 ]] 
+then
+  echo Please provide an element as an argument.
+else
+  echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+fi
+
+
+
+
+
+
