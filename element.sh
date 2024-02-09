dbd874f4d198a5f0a250c07bb31e1eaa7c560dc2
Peter Kwok <peter.kwok94@gmail.com>
2024-02-09 02:34:48 -0500

    feat: null check, subshell test

--- element.sh
+++ element.sh
@@ -9,9 +9,11 @@ else
   ATOMIC_NUMBER_RESULT="$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")"
   SYMBOL_RESULT="$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")"
   NAME_RESULT="$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")"
-  # case
-  # change commit messages
-  echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  if [[ -z $ATOMIC_NUMBER && -z $SYMBOL_RESULT && -z $NAME_RESULT ]]
+  then
+    echo I could not find that element in the database.
+  else
+    # read into variables. group subshell {}
+    echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  fi
 fi
-
-
