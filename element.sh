cc6bedfc8386d856941fc9eb1c2b05655f2c85a4
Peter Kwok <peter.kwok94@gmail.com>
2024-02-10 03:10:35 -0500

    fix: query
    feat: read query result into message

--- element.sh
+++ element.sh
@@ -6,14 +6,22 @@ if [[ ! $1 ]]
 then
   echo Please provide an element as an argument.
 else
-  ATOMIC_NUMBER_RESULT="$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")"
-  SYMBOL_RESULT="$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")"
-  NAME_RESULT="$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")"
-  if [[ -z $ATOMIC_NUMBER && -z $SYMBOL_RESULT && -z $NAME_RESULT ]]
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    PROPERTIES="$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")"
+  else
+    PROPERTIES="$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")"
+  fi
+  
+  if [[ -z $PROPERTIES ]]
   then
     echo I could not find that element in the database.
   else
-    # read into variables. group subshell {}
-    echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+    echo $PROPERTIES | { 
+      IFS="|"
+      read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+    }
   fi
 fi
+
